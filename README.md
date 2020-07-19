0a7be9120df1fd1d17dbbafb604d8d1b557d8f54
Ilya Grishkov <ilyagrishkov@icloud.com>
2020-07-20 00:25:42 +0300

    Add TCP based traceroute

--- README.md
+++ README.md
@@ -21,8 +21,8 @@
 ## <div align="center"> FEATURES </div>
 **Libtraceroute** works at the data link layer with custom-built packets, which provides the user with a lot of options for customizations. The library allows to configure the following parameters:  
 - **_[REQUIRED]_** - Destination address    
-- **_[OPTIONAL]_** - Maximum number of hops &emsp; Port &emsp; Number of queries per hop &emsp; Network interface &emsp;
-- **_[WIP]_** - Timeout per query &emsp; Protocol
+- **_[OPTIONAL]_** - Maximum number of hops &emsp; Port &emsp; Number of queries per hop &emsp; Network interface &emsp; Protocol
+- **_[WIP]_** - Timeout per query 
 
 <br/><br/>
 
--- src/util/packet_builder.rs
+++ src/util/packet_builder.rs
@@ -5,6 +5,7 @@ use pnet::packet::ethernet::{EtherTypes, MutableEthernetPacket};
 use pnet::packet::ip::IpNextHeaderProtocols;
 use pnet::packet::ipv4::MutableIpv4Packet;
 use pnet::packet::udp::MutableUdpPacket;
+use pnet::packet::tcp::MutableTcpPacket;
 use pnet::packet::icmp::{IcmpTypes, IcmpCode, MutableIcmpPacket};
 use pnet::packet::MutablePacket;
 use std::net::Ipv4Addr;
@@ -23,7 +24,7 @@ impl PacketBuilder {
     pub fn build_packet(&self, destination_ip: Ipv4Addr, ttl: u8, port: u16) -> Vec<u8> {
         match self.protocol {
             Protocol::UDP => Self::build_udp_packet(self.source_mac, self.source_ip, destination_ip, ttl, port),
-            Protocol::TCP => unimplemented!(),
+            Protocol::TCP => Self::build_tcp_packet(self.source_mac, self.source_ip, destination_ip, ttl, port),
             Protocol::ICMP => Self::build_icmp_packet(self.source_mac, self.source_ip, destination_ip, ttl)
         }
     }
@@ -86,6 +87,43 @@ impl PacketBuilder {
         icmp_header.set_icmp_code(IcmpCode::new(0));
         icmp_header.set_payload(&[0; 44]);
         icmp_header.set_checksum(pnet::packet::icmp::checksum(&icmp_header.to_immutable()));
+
+        buf.to_vec()
+    }
+
+    /// Create a new TCP packet.
+    fn build_tcp_packet(source_mac: MacAddr, source_ip: Ipv4Addr, destination_ip: Ipv4Addr, ttl: u8, port: u16) -> Vec<u8> {
+        let mut buf = [0u8; 78];
+        let mut mut_ethernet_header = MutableEthernetPacket::new(&mut buf[..]).unwrap();
+
+        mut_ethernet_header.set_destination(MacAddr::zero());
+        mut_ethernet_header.set_source(source_mac);
+        mut_ethernet_header.set_ethertype(EtherTypes::Ipv4);
+
+        let mut ip_header = MutableIpv4Packet::new(mut_ethernet_header.payload_mut()).unwrap();
+
+        ip_header.set_version(4);
+        ip_header.set_header_length(5);
+        ip_header.set_total_length(64);
+        ip_header.set_ttl(ttl);
+        ip_header.set_next_level_protocol(IpNextHeaderProtocols::Tcp);
+        ip_header.set_source(source_ip);
+        ip_header.set_destination(destination_ip);
+        ip_header.set_checksum(pnet::packet::ipv4::checksum(&ip_header.to_immutable()));
+
+        let mut tcp_header = MutableTcpPacket::new(ip_header.payload_mut()).unwrap();
+
+        tcp_header.set_source(rand::thread_rng().gen_range(49152, 65535));
+        tcp_header.set_destination(port);
+        tcp_header.set_sequence(0);
+        tcp_header.set_acknowledgement(0);
+        tcp_header.set_data_offset(5);
+        tcp_header.set_reserved(0);
+        tcp_header.set_flags(0x002);
+        tcp_header.set_window(0);
+        tcp_header.set_payload(&[0; 24]);
+        tcp_header.set_checksum(pnet::packet::tcp::ipv4_checksum(&tcp_header.to_immutable(),
+                                                                 &source_ip, &destination_ip));
 
         buf.to_vec()
     }
